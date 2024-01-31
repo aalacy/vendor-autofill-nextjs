ae3f2411dd0897e3b7323d40298fbbe7ac4f0d37
aalacy <aaronlacywhite387@gmail.com>
2024-01-31 11:04:06 -0800

    chore: small update for ci/cd

--- src/services/vendor-service.js
+++ src/services/vendor-service.js
@@ -3,6 +3,7 @@ import http from "./http";
 export class VendorService {
 
     static all(paginationModel, filterModel, logicOperator) {
+        
         return http.post(`/vendors/all`, {
             page: paginationModel.page+1,
             take: paginationModel.pageSize,
