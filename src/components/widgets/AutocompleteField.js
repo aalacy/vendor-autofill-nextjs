de050e5293948317deca8d2afd7f2d7847236b4d
aalacy <aaronlacywhite387@gmail.com>
2024-04-17 00:47:02 -0700

    fix: automatically fill the to address from the previous from address

--- src/components/widgets/AutocompleteField.js
+++ src/components/widgets/AutocompleteField.js
@@ -5,8 +5,8 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import parse from "autosuggest-highlight/parse";
 import { debounce } from "@mui/material/utils";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { geocodeByPlaceID } from "./geocoder";
 
+import { geocodeByPlaceID } from "./geocoder";
 import { calculateDistance } from "src/utils";
 
 const loadScript = (src, position, id) => {
@@ -30,7 +30,6 @@ export const AutocompleteField = (props) => {
   const [value, setValue] = useState(null);
   const [inputValue, setInputValue] = useState("");
   const [options, setOptions] = useState([]);
-  const [defaultValue, setDefaultValue] = useState(values.data[index][name.split('.').at(-1)] || "");
   const loaded = useRef(false);
   const inputRef = useRef(null);
 
@@ -60,34 +59,38 @@ export const AutocompleteField = (props) => {
     const distance = calculateDistance(fromAddress[0].geometry.location, toAddress[0].geometry.location)
     setFieldValue(`data.${index}.number_of_miles`, distance);
     setFieldValue(`data.${index}.mileage_reimbursement`, distance * .67);
-}, [value, values]);
+  }, [value, values]);
 
-    const manageDefaultValue = () => {
+  const manageDefaultValueForSecondItem = () => {
     if (!values?.data || values.data.length < 2) return;
     if (!name.includes("from_address")) return;
 
-  // "From" address of new milage entry autofills with the "To" address of the previous entry
+    // "From" address of new milage entry autofills with the "To" address of the previous entry
     const curFrom = values.data.at(-1).from_address;
     const curFromPlaceId = values.data.at(-1).from_address_place_id;
-        const prevTo = values.data.at(-2).to_address;
+    const prevTo = values.data.at(-2).to_address;
     const prevToPlaceId = values.data.at(-2).to_address_place_id;
     setFieldValue(`data.${index}.from_address`, curFrom || prevTo);
     setFieldValue(`data.${index}.from_address_place_id`, curFromPlaceId || prevToPlaceId);
-          setDefaultValue(curFrom || prevTo);
-      
-    console.log('curFrom || prevTo', name, curFrom || prevTo)
+    setValue({ place_id: curFromPlaceId || prevToPlaceId, description: curFrom || prevTo })
   }
 
   useEffect(() => {
     if (!values?.data || values.data.length === 0) return;
-        if (!values.data[index].from_address || !values.data[index].to_address) return;
+    if (!values.data[index].from_address || !values.data[index].to_address) return;
     if (!window.google?.maps?.Geocoder) return;
-    
+
     calculateMiles();
   }, [values?.data])
 
   useEffect(() => {
-    manageDefaultValue();
+    manageDefaultValueForSecondItem();
+    const key = name.split('.').at(-1);
+    if (values.data[index].hasOwnProperty(key)) {
+      const description = values.data[index][key];
+      const place_id = values.data[index][`${key}_place_id`];
+      if (place_id && description) setValue({ place_id, description })
+    }
   }, [])
 
   useEffect(() => {
@@ -109,9 +112,9 @@ export const AutocompleteField = (props) => {
       if (active) {
         let newOptions = [];
 
-        // if (value) {
-          //   newOptions = [value];
-        // }
+        if (value) {
+          newOptions = [value];
+        }
 
         if (results) {
           newOptions = [...newOptions, ...results];
@@ -125,6 +128,8 @@ export const AutocompleteField = (props) => {
       const { place_id, description } = value;
       setFieldValue(`${name}_place_id`, place_id);
       setFieldValue(`${name}`, description);
+    } else {
+      
     }
 
     return () => {
@@ -148,9 +153,9 @@ export const AutocompleteField = (props) => {
       options={options}
       filterSelectedOptions
       noOptionsText="No locations"
-      defaultValue={defaultValue}
+      // defaultValue={defaultValue}
       // isOptionEqualToValue={(option, value) => option.id === value.id}
-      // value={value}
+      value={value}
       onChange={(event, newValue) => {
         setOptions(newValue ? [newValue, ...options] : options);
         setValue(newValue);
